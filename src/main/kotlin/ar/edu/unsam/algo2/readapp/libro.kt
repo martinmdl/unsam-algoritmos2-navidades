9dd729501c5fa61bab5ca5c47b7a5d24e19837e2
Cristian Conrado Villafañez Sobrecasa <40326801+KZvilla@users.noreply.github.com>
2024-03-17 15:10:55 -0300

    Definicion de constructor
    Se armo el constructor para la clase libro.

--- src/main/kotlin/ar/edu/unsam/algo2/readapp/libro.kt
+++ src/main/kotlin/ar/edu/unsam/algo2/readapp/libro.kt
@@ -6,5 +6,15 @@ Un libro llega a ser best seller cuando se cumplen todas las siguientes condicio
 Sus ventas semanales son de por lo menos 10.000 unidades.
 Tiene varias ediciones (el criterio actual es más de 2), o muchas traducciones en distintos lenguajes (en este caso no menos de 5).
  */
-class Libro(val nombre: String,val editora: String, val paginas: Int, var ventasSemanales: Double) {
-}
+class Libro(
+    val nombre: String,
+    val editora: String,
+    val paginas: Int,
+    val cantPalabras: Int,
+    var lecturaCompleja: Boolean,
+    var ediciones: Int,
+    var lenguajes: List<Lenguaje>,
+    var ventasSemanales: Double
+) {
+  
+    }
