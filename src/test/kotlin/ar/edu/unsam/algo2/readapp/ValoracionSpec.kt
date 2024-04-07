2ff0360c896c2c1bf92f9ae76ae11f7ad9c69f63
Cristian Villafañez <krisvilla93@gmail.com>
2024-04-07 20:10:24 -0300

    Arreglos de test perdidos.

---
+++ src/test/kotlin/ar/edu/unsam/algo2/readapp/ValoracionSpec.kt
@@ -0,0 +1,111 @@
+@file:Suppress("SpellCheckingInspection")
+
+package ar.edu.unsam.algo2.readapp
+
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+import java.time.LocalDate
+
+class ValoracionSpec:DescribeSpec({
+    describe("Test de Valoracion") {
+        //Arrange
+        /*Autor*/
+        val autor1 = Autor(LocalDate.of(1990, 3, 27), mutableSetOf(),Lenguaje.es_ES,2)
+
+        /*Libro*/
+        val harryPotter = Libro(
+            "HarryPotter",
+            "Salamandra",
+            800,
+            65000,
+            true,
+            5,
+            setOf(Lenguaje.es_ES, Lenguaje.ja_JP),
+            10001,
+            autor1
+        )
+        val harryPotter2 = Libro(
+            "HarryPotter",
+            "Salamandra",
+            800,
+            65000,
+            true,
+            5,
+            setOf(Lenguaje.es_ES, Lenguaje.ja_JP),
+            10001,
+            autor1
+        )
+        val usuario1 = Usuario(
+            "pipo",
+            "alegre",
+            "pipojr10",
+            250,
+            LocalDate.of(1990, 3, 27),
+            "pipo@yahoo.com",
+            mutableSetOf(),
+            mutableMapOf(harryPotter to 1),
+            mutableSetOf(),
+            autor1,
+            mutableSetOf(),
+            mutableSetOf(),
+            precavido,
+            Lenguaje.es_ES,
+            0,
+            0
+        )
+        val usuario2 = Usuario(
+            "pipa",
+            "alegre",
+            "pipojr10",
+            250,
+            LocalDate.of(1990, 3, 27),
+            "pipo@yahoo.com",
+            mutableSetOf(),
+            mutableMapOf(harryPotter2 to 1),
+            mutableSetOf(),
+            autor1,
+            mutableSetOf(),
+            mutableSetOf(),
+            precavido,
+            Lenguaje.es_ES,
+            0,
+            0
+        )
+        val recomendacion1 = Recomendacion(
+            esPrivado = true,
+            creador = usuario1,
+            librosRecomendados = mutableSetOf(harryPotter),
+            descripcion = "no se leer",
+            mutableMapOf()
+        )
+        it("El usuario1 da una valoracion para le recomendacion1"){
+            //Act
+            recomendacion1.crearValoracion(1,"no se leer",usuario1)
+            //Assert
+            (recomendacion1.valoraciones[usuario1]?.comentario ?:String) shouldBe  "no se leer"
+            (recomendacion1.valoraciones[usuario1]?.valor ?: Int) shouldBe  1
+            (recomendacion1.valoraciones[usuario1]?.autor ?: Usuario) shouldBe  usuario1
+        }
+        it("El usuario1 edita el valor numerico de su recomendacion"){
+            //Act
+            recomendacion1.valoraciones[usuario1]?.editarValor(5,usuario1)
+            //Assert
+            (recomendacion1.valoraciones[usuario1]?.valor ?: Int) shouldBe  5
+        }
+        it("El usuario1 edita el comentario de su recomendacion"){
+            //Act
+            recomendacion1.valoraciones[usuario1]?.editarComentario("ahora si se leer",usuario1)
+            //Assert
+            (recomendacion1.valoraciones[usuario1]?.comentario ?: String) shouldBe  "ahora si se leer"
+        }
+        it("El usuario2 da una valoracion para le recomendacion1 por autor favorito"){
+            //Act
+            recomendacion1.crearValoracion(3,"no se leer",usuario2)
+            //Assert
+            (recomendacion1.valoraciones[usuario2]?.comentario ?:String) shouldBe  "no se leer"
+            (recomendacion1.valoraciones[usuario2]?.valor ?: Int) shouldBe  3
+            (recomendacion1.valoraciones[usuario2]?.autor ?: Usuario) shouldBe  usuario2
+        }
+
+    }
+})
