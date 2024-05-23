346bcbd1add163d33da41140159e5b301b3509db
Cristian Villafañez <krisvilla93@gmail.com>
2024-05-23 17:57:51 -0300

    armado de builders

--- src/test/kotlin/ar/edu/unsam/algo2/readapp/RecomendacionObserverSpec.kt
+++ src/test/kotlin/ar/edu/unsam/algo2/readapp/RecomendacionObserverSpec.kt
@@ -2,11 +2,29 @@
 
 package ar.edu.unsam.algo2.readapp
 
-import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.IsolationMode
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
-import org.junit.jupiter.api.assertThrows
+import io.mockk.mockk
 import java.time.LocalDate
 
-class RecomendacionObserverSpec {
-}
+class RecomendacionObserverSpec: DescribeSpec({
+    isolationMode = IsolationMode.InstancePerLeaf
+
+    describe("Test de Observer NotificarCreador") {
+        val mockMailSender = mockk<MailSender>( relaxUnitFun = true)
+        //UsuarioA: Creador de la recomendacion
+    }
+
+    describe("Test de Observer Registro"){
+
+    }
+
+    describe("Test de Observer BannearSpammer"){
+
+    }
+
+    describe("Test de Observer ValoracionAutomatica"){
+
+    }
+})
